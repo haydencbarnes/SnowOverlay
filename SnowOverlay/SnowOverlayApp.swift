6c8a17067a6a5a9bfcc9a152fd5b6032d926c366
haydencbarnes <haydencbarnes@gmail.com>
2024-12-20 08:12:44 -0600

    Initial Commit

---
+++ SnowOverlay/SnowOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowOverlayApp.swift
+//  SnowOverlay
+//
+//  Created by Hayden Barnes on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowOverlayTests/SnowOverlayTests.swift
@@ -0,0 +1,16 @@
+//
+//  SnowOverlayTests.swift
+//  SnowOverlayTests
+//
+//  Created by Hayden Barnes on 12/20/24.
+//
+
+import Testing
+
+struct SnowOverlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
